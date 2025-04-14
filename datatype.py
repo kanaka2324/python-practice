8603dcc33a58e6c6aa65a6964a6eff1b4ed2f2c1
KANAKA <kanakadh2004@gmail.com>
2025-04-14 21:03:09 +0530

    this is my code

---
+++ datatype.py
@@ -0,0 +1,42 @@
+a=10
+print("type of a:",type(a))
+
+a=10
+print("Address a:",id(a))
+
+
+a = 10
+print("value of a :",a)
+print("type of a :",type(a))
+print("Address a:",id(a))
+print("===============================")
+b = 10
+print("value of b :",a)
+print("type of b :",type(a))
+print("Address b:",id(a))
+
+print("is a and b is same",a==b)   
+
+
+f = 100.36
+print("value of f :",f)
+print("type of f :",type(f))
+
+print("===============================")
+
+
+
+
+c = 10+9j
+print("value of c :",c)
+print("type of c :",type(c))
+
+print("===============================")
+ 
+bool= None
+print("value of bool :",bool)
+print("type of bool :",type(bool))
+
+
+from keyword import kwlist
+print(kwlist)
