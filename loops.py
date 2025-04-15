8993534cf350d4c9607f2039e2186e857d406b22
KANAKA <kanakadh2004@gmail.com>
2025-04-15 18:46:59 +0530

    commit

---
+++ loops.py
@@ -0,0 +1,45 @@
+for i in range(5):
+    print(i,"the Hello Python!:")
+
+
+
+    for i in range(1,101):
+      if(i%19==0):
+         print(i,end=" ")
+
+
+i = 1
+while i<=5:
+    print("Hello Python ")
+    i+=1
+
+
+
+i = 1
+while i<=100:
+    if(i%19==0):
+        print(i,end="\n ")
+    i+=1
+
+
+
+    i = 1
+while i<=100:
+    if(i%19==0):
+        print(i,end=" ")
+    i+=1
+
+
+
+print("Hello Java",end=" ")
+print("Hello Python",end=" ")
+
+
+
+day = "21"
+month = "01"
+year = "2025"
+print(day,month,year,sep="-")
+
+
+
