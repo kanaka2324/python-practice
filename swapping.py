9e350bedc2cbb93abfcf49db0048db6bc5ea588b
KANAKA <kanakadh2004@gmail.com>
2025-04-16 21:44:10 +0530

    commit

---
+++ swapping.py
@@ -0,0 +1,49 @@
+a = 10
+b = 20
+print("Before Swapping :",a,b)
+
+a = 10
+b = 20
+temp = a 
+a = b 
+b = temp
+print("After Swapping ",a,b)
+
+
+
+a = 10
+b = 20
+print("Before Swapping ",a,b)
+
+a = a + b 
+b = a - b 
+a = a - b 
+print("After Swapping ",a,b)
+
+
+a = 10
+b = 20
+print("Before Swapping ",a,b)
+
+a = a  * b 
+b = a // b 
+a = a // b 
+print("After Swapping ",a,b)
+
+
+a = a ^ b 
+b = a ^ b 
+a = a ^ b
+print("After Swapping ",a,b)
+   
+
+"""   
+a = 10
+b = 20
+print("Before Swapping ",a,b)
+
+a,b = b,a
+print("After Swapping ",a,b)
+"""
+
+
